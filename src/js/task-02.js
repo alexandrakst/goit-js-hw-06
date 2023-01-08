9ae6baf86fa05a726af4e1b78f0963d65f148296
alexandrakst <alexandra.kostenko@hotmail.com>
2023-01-08 18:20:37 -0500

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+console.log(ingredientsList);
+
+for (const ingredient of ingredients) {
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = ingredient;
+  ingredientsList.append(list);
+}
