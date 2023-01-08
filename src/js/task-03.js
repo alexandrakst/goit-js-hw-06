655ac799c8bfdd9dfa2c99a03d6ad6f72428f39d
alexandrakst <alexandra.kostenko@hotmail.com>
2023-01-08 18:47:51 -0500

    task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const imageItem = images.map(
+  image => `<li> <img src = ${image.url} alt = ${image.alt} width = 300 height = 200 /> </li>`
+);
+
+gallery.classList.add('img-list');
+
+gallery.insertAdjacentHTML('beforebegin', imageItem);
