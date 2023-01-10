13dd96a819288203599856eff15173da2f8f1f19
alexandrakst <alexandra.kostenko@hotmail.com>
2023-01-09 22:56:35 -0500

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const buttonColor = document.querySelector('.change-color');
+console.log(buttonColor);
+buttonColor.addEventListener('click', changeColor);
+
+const spanColor = document.querySelector('.color');
+console.log(spanColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanColor.textContent = color;
+}
